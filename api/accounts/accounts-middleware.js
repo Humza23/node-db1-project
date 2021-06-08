b66e57c5bad6f699e563745cd908926286040449
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-06-08 00:48:31 -0400

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,24 +2,18 @@ const Account = require('./accounts-model')
 const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
-  const error = { status: 400}
   if (req.body.name === undefined || req.body.budget === undefined) {
-    error.message = 'name and budget are required'
+    return res.status(400).json({ message: 'name and budget are required'})
   } else if (typeof req.body.name !== 'string') {
-    error.message = 'name of account must be a string'
+    return res.status(400).json({ message: 'name of account must be a string'})
   } else if (req.body.name.trim().length < 3 || req.body.name.trim().length > 100) {
-    error.message = "name of account must be between 3 and 100" 
+    return res.status(400).json({ message: 'name of account must be between 3 and 100'})
   } else if (typeof req.body.budget !== 'number' || isNaN(req.body.budget)) {
-    error.message = "budget of account must be a number" 
+    return res.status(400).json({ message: 'budget of account must be a number'})
   } else if (req.body.budget < 0 || req.body.budget > 1000000) {
-    error.message = "budget of account is too large or too small" 
+    return res.status(400).json({ message: 'budget of account is too large or too small'})
   }
-
-  if (error.message){
-    next(error)
-  } else {
   next()
-}
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
@@ -28,7 +22,7 @@ exports.checkAccountNameUnique = async (req, res, next) => {
     .where('name', req.body.name.trim()).first()
     
     if (existing) {
-      next({status: 400, message: 'that name is taken' })
+      return res.status(400).json({message: 'that name is taken' })
     } else {
       next()
     }
@@ -41,7 +35,7 @@ exports.checkAccountId = async (req, res, next) => {
   try {
     const account = await Account.getById(req.params.id)
     if (!account) {
-      next({ status: 404, message: 'account not found'})
+      return res.status(404).json({message: 'account not found'})
     } else {
       req.account = account
       next()
